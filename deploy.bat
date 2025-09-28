8d56675d407247ab013f251905bcb71843e4c8a8
Snakefishh <ckv8@mail.ru>
2025-09-28 16:06:14 +0700

    del deploy

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-del ConfData.zip
-"C:\Program Files\7-Zip\7z.exe" a -tzip -mcu ConfData.zip *\*.txt
-git add .
-git commit -m "%date% %time:~0,5%"
-git push https://snakefishh2@github.com/snakefishh2/ConfData.git
